b503d059f6bca8060951c5a61ae1b618ae00dcb3
Eras256 <ticketsafe4@gmail.com>
2026-03-05 22:11:09 -0600

    chore: remove test files causing build error

--- apps/web/test_swk.ts
+++
@@ -1,3 +0,0 @@
-import { StellarWalletsKit } from "@creit.tech/stellar-wallets-kit";
-import { defaultModules } from '@creit.tech/stellar-wallets-kit/modules/utils';
-console.log(StellarWalletsKit.init ? "HAS INIT" : "NO INIT");
--- apps/web/test_swk2.ts
+++
@@ -1,5 +0,0 @@
-import { StellarWalletsKit } from "@creit.tech/stellar-wallets-kit";
-import { defaultModules } from '@creit.tech/stellar-wallets-kit/modules/utils';
-import { FreighterModule, FREIGHTER_ID } from '@creit.tech/stellar-wallets-kit/modules/freighter';
-
-console.log("Freighter ID is", FREIGHTER_ID);
